fc6c275be3f861bf430d5ab04f875a0d2dc3e11b
zhuzichu520 <zhuzichu520@gmail.com>
2020-05-22 17:14:11 +0800

    添加 qmui

---
+++ widget/src/main/java/com/hiwitech/android/widget/qmui/span/QMUIOnSpanClickListener.java
@@ -0,0 +1,10 @@
+package com.hiwitech.android.widget.qmui.span;
+
+/**
+ * @author cginechen
+ * @date 2017-03-20
+ */
+
+public interface QMUIOnSpanClickListener {
+    boolean onSpanClick(String text);
+}
