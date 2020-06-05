59d0ceddad299202011d7028474eeab560ba5c70
zhuzichu520 <zhuzichu520@gmail.com>
2020-06-05 11:16:36 +0800

    ToolIntent 添加emial text market跳转

--- app/src/main/kotlin/com/hiwitech/android/developer/ui/category/fragment/FragmentCategory.kt
+++ app/src/main/kotlin/com/hiwitech/android/developer/ui/category/fragment/FragmentCategory.kt
@@ -5,7 +5,9 @@ import com.hiwitech.android.developer.R
 import com.hiwitech.android.developer.base.FragmentBase
 import com.hiwitech.android.developer.databinding.FragmentCategoryBinding
 import com.hiwitech.android.developer.ui.category.viewmodel.ViewModelCategory
+import com.hiwitech.android.libs.tool.jumpMarket
 import com.hiwitech.android.mvvm.base.ArgDefault
+import kotlinx.android.synthetic.main.fragment_category.*
 
 class FragmentCategory :
     FragmentBase<FragmentCategoryBinding, ViewModelCategory, ArgDefault>() {
@@ -13,4 +15,13 @@ class FragmentCategory :
     override fun bindVariableId(): Int = BR.viewModel
 
     override fun setLayoutId(): Int = R.layout.fragment_category
+
+    override fun initListener() {
+        super.initListener()
+        click.setOnClickListener {
+//            jumpShareText(requireContext(),"分享文本到","一剑凌情愿")
+//            jumpEmail(requireContext(),"emial打开","524787275@qq.com")
+            jumpMarket(requireContext(),"Market")
+        }
+    }
 }
--- libs/src/main/java/com/hiwitech/android/libs/tool/ToolIntent.kt
+++ libs/src/main/java/com/hiwitech/android/libs/tool/ToolIntent.kt
@@ -14,6 +14,7 @@ import androidx.annotation.IntRange
 import androidx.core.content.FileProvider
 import java.io.File
 
+
 /**
  * 启动下一个Activity
  *
@@ -332,6 +333,67 @@ fun jumpToGooglePlay(ctx: Context?, packageName: String?): Boolean {
 }
 
 /**
+ * 分享文本
+ * @param ctx 上下文
+ * @param title 标题
+ * @param text 分享的内容
+ */
+fun jumpShareText(ctx: Context?, title: String, text: String): Boolean {
+    if (ctx == null)
+        return false
+    return try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+        intent.type = "text/plain"
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        ctx.startActivity(Intent.createChooser(intent, title))
+        true
+    } catch (e: Exception) {
+        false
+    }
+}
+
+/**
+ * 跳转至email
+ * @param ctx 上下文
+ * @param title 标题
+ * @param text 分享的emial
+ */
+fun jumpEmail(ctx: Context?, title: String, email: String): Boolean {
+    if (ctx == null)
+        return false
+    return try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        ctx.startActivity(Intent.createChooser(intent, title))
+        true
+    } catch (e: Exception) {
+        false
+    }
+}
+
+/**
+ * 跳转至应用商场
+ * @param ctx 上下文
+ * @param title 标题
+ */
+fun jumpMarket(ctx: Context?, title: String): Boolean {
+    if (ctx == null)
+        return false
+    return try {
+        val uri = Uri.parse("market://details?id=" + ctx.packageName)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        ctx.startActivity(Intent.createChooser(intent, title))
+        true
+    } catch (e: Exception) {
+        false
+    }
+}
+
+/**
  * 根据Intent判断，是否存在该Activity<br/>
  * 为了跳转之前做判断，防止崩溃
  *
@@ -342,3 +404,4 @@ fun jumpToGooglePlay(ctx: Context?, packageName: String?): Boolean {
 fun hasActivityByIntent(ctx: Context, intent: Intent): Boolean {
     return intent.resolveActivity(ctx.packageManager) != null
 }
+
